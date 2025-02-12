2dfc1e9a0f19b0045e737834d82425a8fb534df7
Tim Hrdy <tim.hrdy.1@gmail.com>
2025-02-12 01:45:25 +0100

    latest+strapi

--- src/app/components/front-office/home/home.component.ts
+++ src/app/components/front-office/home/home.component.ts
@@ -1,12 +1,19 @@
-import {Component, signal} from '@angular/core';
-import {MatCardModule} from '@angular/material/card';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, signal } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { ChangeImgButtonDirective } from '../../../directives/img-button/change-img-button.directive';
 import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {CdkAccordionModule} from '@angular/cdk/accordion';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { CdkAccordionModule } from '@angular/cdk/accordion';
+import { StrapiService } from '../../../services/strapi.service';
 
+interface FAQItem {
+  title: string;
+  content: string;
+}
 
 @Component({
   selector: 'app-home',
@@ -21,39 +28,42 @@ import {CdkAccordionModule} from '@angular/cdk/accordion';
     CdkAccordionModule
   ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
   progressValue = 1;
   
-    steps = [
-      { label: 'Je trouve ma solution', img: 'assets/images/home-progress/01.svg' },
-      { label: 'Je signe ma proposition tarifaire électroniquement', img: 'assets/images/home-progress/02.svg' },
-      { label: 'D&D effectue la collecte dans les délais les plus courts', img: 'assets/images/home-progress/03.svg' },
-      { label: 'Votre procès verbal d’intervention envoyé automatiquement sous 48h ouvrés après l’intervention', img: 'assets/images/home-progress/04.svg' },
-      { label: 'Vous participer pour une planète durable', img: 'assets/images/home-progress/05.svg' }
-    ];
+  steps = [
+    { label: 'Je trouve ma solution', img: 'assets/images/home-progress/01.svg' },
+    { label: 'Je signe ma proposition tarifaire électroniquement', img: 'assets/images/home-progress/02.svg' },
+    { label: 'D&D effectue la collecte dans les délais les plus courts', img: 'assets/images/home-progress/03.svg' },
+    { label: 'Votre procès verbal d’intervention envoyé automatiquement sous 48h ouvrés après l’intervention', img: 'assets/images/home-progress/04.svg' },
+    { label: 'Vous participer pour une planète durable', img: 'assets/images/home-progress/05.svg' }
+  ];
 
-    stepIcone = [{img: 'assets/images/home-progress/01.svg'}];
-  
-    updateProgress(stepIndex: number) {
-      this.progressValue = stepIndex * 20;
-    }
+  stepIcone = [{img: 'assets/images/home-progress/01.svg'}];
 
-    readonly panelOpenState = signal(false);
-  
-      items = [
-        { title: 'Quels types de prestations proposez-vous?', 
-          content: 'D&D propose divers services de destruction de documents et disques durs pour protéger les organisations\
-           contre les risques de violation des informations.\
-          Nous proposons aussi la collecte et le recyclage d\'archives et de documents papiers de bureau. Voici quelques-uns\
-          de nos services les plus populaires : Lien vers la pages des services' },
-        { title: 'Pourquoi avoir recours à un service de collecte et de destruction?',
-          content: 'Contenu spécifique pour l\'Item 2' },
-        { title: 'Quel type de matériaux prenez-vous en charge?', content: 'Contenu spécifique pour l\'Item 3' },
-        { title: 'Comment dois-je préparer lintervention de D&D dans mes locaux?', content: 'Contenu spécifique pour l\'Item 3' },
-        { title: 'Que deviennent les matériaux détruits?', content: 'Contenu spécifique pour l\'Item 3' }
-      ];
-    
+  readonly panelOpenState = signal(false);
+
+  items: FAQItem[] = [];
+
+  constructor(private strapiService: StrapiService) {}
+
+  ngOnInit() {
+    this.fetchItems();
+  }
+
+  fetchItems() {
+    this.strapiService.getFaqs().subscribe(response => {
+      this.items = response.data.map((item: any) => ({
+        title: item.question,
+        content: item.anwser.map((ans: any) => ans.children.map((child: any) => child.text).join('')).join('\n')
+      }));
+    });
+  }
+
+  updateProgress(stepIndex: number) {
+    this.progressValue = stepIndex * 20;
+  }
 }
