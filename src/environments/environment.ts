9bdeb82d39a7ef29935634c105a6d95e589342d6
Tim Hrdy <tim.hrdy.1@gmail.com>
2025-02-12 01:46:59 +0100

    env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,5 +7,6 @@ export const environment = {
   keyApiSpring: 'Hlpwy7thiOyZY3cr3RCtmM1BRwKn8UTYCUj7uWNJpKMeXJouu7iYPBmr6WJTaG2L',
   apiSirenUrl: '/api-siren',
   apiBaseUrl: 'http://ns100.evxonline.net:8000/therefore',
-  tokenApiSiren:'Hlpwy7thiOyZY3cr3RCtmM1BRwKn8UTYCUj7uWNJpKMeXJouu7iYPBmr6WJTaG2L'
+  tokenApiSiren:'Hlpwy7thiOyZY3cr3RCtmM1BRwKn8UTYCUj7uWNJpKMeXJouu7iYPBmr6WJTaG2L',
+  strapiUrl: 'http://localhost:1337'
 };
